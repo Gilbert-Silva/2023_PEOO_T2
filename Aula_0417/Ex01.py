9f95b36e7fcb55b4016fca47b75e481a4e8c5636
Replit user <>
2023-04-17 14:43:28 +0000

    Aula 17_04

---
+++ Aula_0417/Ex01.py
@@ -0,0 +1,15 @@
+x = 1
+while x <= 10:
+  x += 1
+  print(x)
+
+print("Aqui!", x)
+
+s = 0 # Somatório inicia com zero
+n = 1 # Primeiro elemento
+while n < 20: # Teste
+  print(n) # Mostra o valor
+  s += n # Soma o valor no total
+  n += 2 # Atualiza o valor
+print(f"Soma = {s}")
+print(n)
---
+++ Aula_0417/Ex02.py
@@ -0,0 +1,12 @@
+print(5)
+x = []
+print(x)
+x = x + [10]
+print(x)
+x = x + [5]
+print(x)
+x.append(15)
+x.append(12)
+print(x)
+for i in x: print(i)
+  
