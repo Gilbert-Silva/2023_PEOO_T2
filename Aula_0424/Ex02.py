20625047981e4bb41ec3778db1f97189966e1470
Gilbert-Silva <gilbertif1995@outlook.com>
2023-04-24 14:50:06 +0000

    Aula 2404

---
+++ Aula_0424/Ex02.py
@@ -0,0 +1,46 @@
+print("Teste")
+x = 5
+print(x)
+print(type(x))
+y = 2.5
+print(y)
+print(type(y))
+"""
+z = 'Teste'
+print(z)
+print(type(z))
+"""
+
+
+#a = input()
+#print(a)
+#x = int(input())
+#print(x + x)
+#y = float(input())
+#print(y + y)
+
+x = int(input())
+if x > 5:
+  print("maior que 5")
+elif x < 5:
+  print("menor que 5")
+else:
+  print("igual a 5")
+    
+x = 1
+while x <= 10: 
+  print(x)
+  x += 1 
+
+s = "Teste"
+for c in s:
+  print(c)
+
+for x in range(1, 11):
+  print(x)
+
+&& and
+|| or
+! not 
+
+
