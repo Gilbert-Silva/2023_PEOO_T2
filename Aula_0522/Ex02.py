b94295580df7f32305c81c54d961f32a4effbf36
Replit user <>
2023-05-22 14:42:07 +0000

    Aula 2205

---
+++ Aula_0522/Ex02.py
@@ -0,0 +1,22 @@
+x = True
+print(type(x))
+y = False
+print(type(y))
+
+x = 5 > 10
+print(x)
+
+def é_par(x):
+  if x % 2 == 0: return True
+  else: return False
+
+def é_impar(x):
+  return x % 2 == 1
+
+print(é_par(20))
+print(é_impar(21))
+
+
+
+
+
