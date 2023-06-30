16f7568f2e05eab356f0c9c522ba9d5e40039e7a
Replit user <>
2023-06-30 13:27:48 +0000

    Time

--- Aula_0630/Ex03.py
+++ Aula_0630/Ex03.py
@@ -1,38 +1,48 @@
 class Jogador:
+
   def __init__(self, nome, gols):
     self.__nome = nome
     self.__gols = gols
+
   def get_gols(self):
     return self.__gols
+
   def __str__(self):
     return f"{self.__nome} - {self.__gols} gol(s)"
 
+
 class Time:
+
   def __init__(self, nome):
     self.__nome = nome
     self.__jogadores = []
+
   def inserir(self, j):
     self.__jogadores.append(j)
+
   def listar(self):
     return self.__jogadores
+
   def artilheiro(self):
     if len(self.__jogadores) == 0: return None
     art = self.__jogadores[0]
     for j in self.__jogadores:
       if j.get_gols() > art.get_gols():
         art = j
-      return art  
-    
+      return art
+
+
 class UI:
+
   @staticmethod
   def menu():
     print("0-Fim, 1-Inserir, 2-Listar, 3-Artilheiro")
     return int(input())
-  
+
   @staticmethod
   def main():
     op = 10
-    x = Time("IFRN")    
+    x = Time("IFRN")
     while op != 0:
       op = UI.menu()
       if op == 1:
@@ -40,9 +50,19 @@ class UI:
         gols = int(input("Gols: "))
         j = Jogador(nome, gols)
         x.inserir(j)
-      if op == 2:  
-        for j in x.listar(): print(j)
-      if op == 3:  
+      if op == 2:
+        for j in x.listar():
+          print(j)
+      if op == 3:
         print(x.artilheiro())
 
-UI.main()
+
+#UI.main()
+a = Jogador("um", 10)
+b = Jogador("dois", 20)
+x = Time("IFRN")
+x.inserir(a)
+x.inserir(b)
+for j in x.listar():
+  print(j)
+print(x.artilheiro())
